cd19023609c37e3ee19f159957529d974fa779f2
Kacper Gajewski <kacper.gajewski@op.pl>
2022-11-24 00:19:04 +0100

    end landing page layout and styling for standard mobile

--- src/components/Pages/LandingPage/LandingPage.styles.ts
+++ src/components/Pages/LandingPage/LandingPage.styles.ts
@@ -1,14 +1,21 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import {
+  TbCircle1,
+  TbCircle2,
+  TbCircle3,
+  TbCircle4,
+  TbCircle5,
+} from "react-icons/tb";
 
 export const LandingTitle = styled.section`
   width: 100%;
-  min-height: 30rem;
+  min-height: 40rem;
   display: flex;
   position: relative;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  color: #2683a8;
+  color: #d8d8d8;
   text-shadow: 2px 2px 15px #0f0f0f;
   background-color: #3d3d3d;
   clip-path: polygon(0 0, 100% 0%, 100% 90%, 0 100%);
@@ -22,50 +29,51 @@ export const LandingTitle = styled.section`
     content: "";
     display: block;
     width: 100%;
-    min-height: 30rem;
+    min-height: 40rem;
     position: absolute;
     background: #00000075;
   }
 `;
 
 export const TitleIcon = styled.span`
-  z-index: 20;
-  position: absolute;
-  top: 5.5rem;
+  z-index: 10;
 
   & > svg {
-    margin-top: 0.5rem;
+    margin-top: 1rem;
     font-size: 3rem;
-    color: #287a9b;
+    color: #d8d8d8;
   }
 `;
 
 export const LandingWelcome = styled.section`
   width: 100%;
-  min-height: 22rem;
+  min-height: 30rem;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
   margin-top: 2rem;
+  padding: 2rem 0;
 `;
 
 export const LandingDescription = styled.section`
   width: 100%;
   position: relative;
-  min-height: 50rem;
-  clip-path: polygon(0 10%, 100% 0%, 100% 100%, 0 100%);
+  min-height: 58rem;
+  clip-path: polygon(0 7%, 100% 0%, 100% 93%, 0% 100%);
   background-image: url(./images/LandingDesc.jpg);
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 
   &::after {
     content: "";
-    min-height: 50rem;
-    width: 100%;
     display: block;
     width: 100%;
+    min-height: 58rem;
     position: absolute;
     background: #00000075;
   }
@@ -76,22 +84,126 @@ export const Handler = styled.div`
   flex-direction: column;
   align-items: center;
   width: 100%;
-  height: fit-content;
+  height: 10rem;
 
   & > h1 {
-    margin-top: 2rem;
+    margin-top: 3rem;
     z-index: 15;
+  }
+
+  &:last-child {
+    margin-bottom: 4rem;
   }
 `;
 
 export const Slogan = styled.p`
-  color: #2683a8;
+  color: #d8d8d8;
   font-size: 1.3rem;
   font-style: italic;
   font-weight: bold;
   letter-spacing: 2px;
-  text-shadow: 2px 2px 15px #0f0f0f;
+  text-shadow: 5px 5px 15px #0f0f0f;
   padding: 0 1.5rem;
   margin-bottom: 5rem;
   z-index: 15;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  z-index: 10;
+  margin-top: 5rem;
+`;
+
+const iconStyle = css`
+  font-size: 4rem;
+  color: #d8d8d8;
+`;
+
+export const First = styled(TbCircle1)`
+  ${iconStyle};
+`;
+
+export const Second = styled(TbCircle2)`
+  ${iconStyle};
+`;
+
+export const Third = styled(TbCircle3)`
+  ${iconStyle};
+`;
+
+export const Fourtf = styled(TbCircle4)`
+  ${iconStyle};
+`;
+
+export const Fifth = styled(TbCircle5)`
+  ${iconStyle};
+`;
+
+export const UnderlineParagraph = styled.p`
+  color: #d8d8d8;
+  width: 100%;
+  text-align: center;
+  font-size: 1.3rem;
+  font-style: italic;
+  letter-spacing: 2px;
+  padding: 0 1.5rem;
+  margin-top: 1.5rem;
+`;
+
+export const JoinSection = styled.section`
+  width: 100%;
+  min-height: 30rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: -4rem;
+  padding: 4rem 0;
+`;
+
+const buttonStyles = css`
+  margin-top: 1rem;
+  font-size: 1.8rem;
+  padding: 0.5rem 1rem;
+  border-radius: 20px;
+  background: transparent;
+  transition: 0.3s;
+  z-index: 10;
+
+  &:focus-visible {
+    outline: none;
+    border: 2px solid #d8d8d8;
+    color: #d8d8d8;
+  }
+`;
+
+export const SignBtn = styled.button`
+  ${buttonStyles}
+
+  border: 2px solid #7fa1ce;
+  color: #7fa1ce;
+
+  &:hover {
+    cursor: pointer;
+    border: 2px solid #2683a8;
+    color: #d8d8d8;
+  }
+`;
+
+export const LoginBtn = styled.button`
+  ${buttonStyles}
+
+  border: 2px solid #d8d8d8;
+  color: #d8d8d8;
+  margin-bottom: 2rem;
+
+  &:hover {
+    cursor: pointer;
+    border: 2px solid #e2a9a9;
+    color: #e2a9a9;
+  }
 `;
--- src/components/Pages/LandingPage/LandingPage.tsx
+++ src/components/Pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,27 @@
 import { GiBoatFishing } from "react-icons/gi";
-import { Paragraph, PrimaryTitle, SecondaryTitle } from "../../../helpers/main.styles";
+
+import {
+  Paragraph,
+  PrimaryTitle,
+  SecondaryTitle,
+} from "../../../helpers/main.styles";
 import Modal from "../../UI/Modal";
-import { LandingWelcome, LandingTitle, TitleIcon, LandingDescription, Handler, Slogan } from "./LandingPage.styles";
+import {
+  LandingWelcome,
+  LandingTitle,
+  TitleIcon,
+  LandingDescription,
+  Handler,
+  Slogan,
+  DescriptionContainer,
+  First,
+  Second,
+  Third,
+  UnderlineParagraph,
+  JoinSection,
+  SignBtn,
+  LoginBtn,
+} from "./LandingPage.styles";
 
 const LandingPage = () => {
   return (
@@ -9,19 +29,57 @@ const LandingPage = () => {
       <LandingTitle>
         <Handler>
           <PrimaryTitle>FishingApp</PrimaryTitle>
-          <TitleIcon><GiBoatFishing/></TitleIcon>
+          <TitleIcon>
+            <GiBoatFishing />
+          </TitleIcon>
         </Handler>
         <Handler>
+          <LoginBtn>Log in</LoginBtn>
           <Slogan>The best app for anglers!</Slogan>
         </Handler>
         {/* The best app for anglers! */}
       </LandingTitle>
       <LandingWelcome>
         <SecondaryTitle>Welcome in our App!</SecondaryTitle>
-        <Paragraph>We are sure you will find many well-stocked places with fish here. You will no longer have to worry about not catching any fish!</Paragraph>
-        <Paragraph>How You can use our App? It's trivial, below you will find a brief description of the functionality!</Paragraph>
+        <Paragraph>
+          We are sure you will find many well-stocked places with fish here. You
+          will no longer have to worry about not catching any fish!
+        </Paragraph>
+        <Paragraph>
+          How You can use our App? It's trivial, below you will find a brief
+          description of the functionality!
+        </Paragraph>
       </LandingWelcome>
-      <LandingDescription></LandingDescription>
+      <LandingDescription>
+        <DescriptionContainer>
+          <First />
+          <UnderlineParagraph>
+            You can search for locations of fish shoals marked by users
+          </UnderlineParagraph>
+        </DescriptionContainer>
+        <DescriptionContainer>
+          <Second />
+          <UnderlineParagraph>
+            If you are logged-in user, you can indicate yourself your favorite
+            place where the fish shoal is currently located
+          </UnderlineParagraph>
+        </DescriptionContainer>
+        <DescriptionContainer>
+          <Third />
+          <UnderlineParagraph>
+            For logged-in users, you can see your recently added places with a
+            description of the catch, which you left
+          </UnderlineParagraph>
+        </DescriptionContainer>
+      </LandingDescription>
+      <JoinSection>
+        <SecondaryTitle>Want to Join us?</SecondaryTitle>
+        <Paragraph>
+          You can join us for free! If you don't have an account, click the
+          register button
+        </Paragraph>
+        <SignBtn>Sing in</SignBtn>
+      </JoinSection>
     </Modal>
   );
 };
